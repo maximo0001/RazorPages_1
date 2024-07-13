18f42528ffb00969425487c22ea73b2f2e3c1828
Maximo Fernandez <maximo148601@gmail.com>
2024-07-13 00:14:46 -0300

    razor page with scaffolding

---
+++ WebApp1/WebApp1/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApp1.Data;
+using WebApp1.Models;
+
+namespace WebApp1.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly WebApp1.Data.WebApp1Context _context;
+
+        public CreateModel(WebApp1.Data.WebApp1Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ WebApp1/WebApp1/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApp1.Data;
+using WebApp1.Models;
+
+namespace WebApp1.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebApp1.Data.WebApp1Context _context;
+
+        public IndexModel(WebApp1.Data.WebApp1Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
